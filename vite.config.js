60dbd004daaadf6f379b0c2dcfce1297223cad59
Vl-pod <110475023+Vl-pod@users.noreply.github.com>
2024-04-10 02:35:16 +0400

    change vite.config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
+  base: "/xcrwd.lending",
+	plugins: [
     vue(),
     vueJsx(),
   ],
