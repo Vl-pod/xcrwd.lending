4f3f34a0ff4a899454beec20dec65126bf827856
ultratestbro <xxai@xxai.live>
2024-04-23 15:08:59 +0400

    add tickerbox

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,14 +1,17 @@
 <template>
 	<div class="page">
     <IntroContent />
+    <TickerBox />
 	</div>
 </template>
 
 <script>
 import IntroContent from '../components/IntroContent.vue'
+import TickerBox from  '../components/TickerBox.vue'
 export default {
   components: {
     IntroContent,
+    TickerBox
   },
   data() {
     return {}
